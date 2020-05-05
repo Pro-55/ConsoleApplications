f134dc42af67139e0d68e9723758494880973246
Pranit Rane <rane.pranit.b@gmail.com>
2020-05-05 20:41:15 +0530

    1. CatDog 2. CleanString

---
+++ Kotlin/CatDog.kt
@@ -0,0 +1,29 @@
+package CatDog 
+
+import java.util.*
+import java.lang.*
+
+fun main(args: Array<String>) { 
+  val scan = Scanner(System.`in`)
+  var shouldRepeat: Boolean
+  do{
+    println()
+    println("Enter a string:")
+  	var input = scan.nextLine()
+    val isValid = cat_dog(input)
+    println("$input -> $isValid")
+  
+    println("Want to go again? (y/n)")
+    var choice = scan.nextLine() 
+    shouldRepeat = choice.equals("y", true)
+  } while(shouldRepeat)
+  println("</---------------------------------------------------------------------->")
+}
+
+fun cat_dog(input:String): Boolean {
+  val catCount = countSubstring(input, "cat")
+  val dogCount = countSubstring(input, "dog")
+  return catCount == dogCount 
+}
+
+fun countSubstring(input: String, sub: String): Int = input.split(sub).size - 1
---
+++ Kotlin/CleanString.kt
@@ -0,0 +1,27 @@
+package CleanString 
+
+import java.util.*
+import java.lang.*
+
+fun main(args: Array<String>) { 
+  val scan = Scanner(System.`in`)
+  var shouldRepeat: Boolean
+  do{
+    println()
+    println("Enter a string:")
+  	var input = scan.nextLine()
+    val cleanString = stringClean(input)
+    println("$input -> $cleanString")
+  
+    println("Want to go again? (y/n)")
+    var choice = scan.nextLine() 
+    shouldRepeat = choice.equals("y", true)
+  } while(shouldRepeat)
+  println("</---------------------------------------------------------------------->")
+}
+
+fun stringClean(input:String): String {
+  var output = ""
+  input.forEach{ if(!output.contains(it)) output += it }
+  return output 
+}
